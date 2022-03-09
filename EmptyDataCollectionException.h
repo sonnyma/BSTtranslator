dea301915f0fe9d4091952b2162a2b4d585d8c38
Sonny Ma <93969957+sonnyma@users.noreply.github.com>
2022-03-08 19:08:40 -0800

    Create EmptyDataCollectionException.h

---
+++ EmptyDataCollectionException.h
@@ -0,0 +1,22 @@
+/*
+ * EmptyDataCollectionException.h
+ *
+ * Class Description: Defines the exception that is thrown when the data collection is empty.
+ *
+ * Author: Inspired from our textbook's authors Frank M. Carrano and Tim Henry.
+ *         Copyright (c) 2013 __Pearson Education__. All rights reserved.
+ */
+ 
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+class EmptyDataCollectionException : public logic_error
+{
+public:
+   EmptyDataCollectionException(const string& message = "");
+   
+}; // end EmptyDataCollectionException 
